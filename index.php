6fa61ac751d0992aeceb80e95f1c7014628e3cb6
gustavo-010 <70542602+gustavo-010@users.noreply.github.com>
2020-09-03 21:05:04 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>HTML</title>
+  
+  <!-- HTML -->
+  
+
+  <!-- Custom Styles -->
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <div class="wrapper">
+  <div class="wrapper_child">
+    <div id="home" class="tab">
+      <h3 class="text-center align-middle">Home</h3>
+    </div>
+
+    <div id="gallery" class="tab">
+      <h3 class="text-center align-middle">Gallery</h3>
+    </div>
+
+    <div id="about" class="tab">
+      <h3 class="text-center align-middle">About us</h3>
+    </div>
+
+    <div id="contacts" class="tab">
+      <h3 class="text-center align-middle">Contacts</h3>
+    </div>
+  </div>
+</div>
+  <script src="main.js"></script>
+</body>
+</html>
